90d6fde455feb7f3d8140390cf2a6912632e05f7
Alyce Sousa <alyce.cristine.s@gmail.com>
2022-07-21 17:05:12 -0300

    fazendo upload do projeto

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img
+          src="https://i.pinimg.com/736x/ca/e5/94/cae5943ada4d615afbc63a87f004e1fb.jpg"
+          style={{ width: 200 }}
+          alt="logo"
+        />
+        <h1>SUPERMAN</h1>
+        <p>ele mora no povoado de smallvile</p>
+        <div>
+          <p>
+            seus poderes são: voo, visão raio X, visão de calor, super sopro,
+            força descomunal, super audição e ivunerabilidade.
+          </p>
+        </div>
+        <div>
+          <h3>História</h3>
+          <p>
+            O personagem nasceu no planeta fictício Krypton e tinha o nome de
+            Kal-El. Foi enviado à Terra por seu pai cientista, Jor-El, momentos
+            antes de Krypton explodir. O foguete aterrissou na cidade de
+            Smallville.
+          </p>
+        </div>
+      </div>
+      <p>criador: Kalel P.</p>
+    </div>
+  );
+}
+
+export default App;

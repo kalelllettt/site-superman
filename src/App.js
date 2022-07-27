3d68bd3fe758a21e44dfa8425a80457b990dfd32
Alyce Sousa <alyce.cristine.s@gmail.com>
2022-07-27 16:54:29 -0300

    Atualização 1.1

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
           <p>
             Povoado de Sallvile, que foca no condado de de Lowell, Kansas, EUA,
             fundada por Ezra Small. Clark Kent viveu na cidade desde que foi
-            adotado por Jonathan e Martha Kent. Smallville está localizada a uma
-            curta distância de Metropolis, 425 milhas de Hub City e perto também
-            de Grandville.
+            adotado por Jonathan e Martha Kent. Smallville está localizada a
+            umaP curta distância de Metropolis, 425 milhas de Hub City e perto
+            também de Grandville.
           </p>
           <h3>Poderes:</h3>
           <p>
@@ -25,6 +25,24 @@ function App() {
             Superman tem diversos poderes sobre-humanos: pode voar, tem força
             descomunal, visão de raio-x, visão de calor, supersopro,
             superaudição e invunerabilidade.
+          </p>
+        </div>
+        <div>
+          <h3>Porque o Superman tem um S no peito?</h3>
+          <p>
+            Alguns simplesmente acham que é a inicial para “Supeman”. No filme
+            mais recente, no entanto, quando questionado por Lois Lane qual era
+            o significado da inicial, Clark Kent revela que aquilo não era um
+            “S”, e sim um símbolo “kryptoniano” para “esperança”
+          </p>
+        </div>
+        <div>
+          <h3>Vilões clàssicos do Superman:</h3>
+          <p>
+            Lex Luthor, Bizarro, Darkseid, Ultraman, Solomon Grundy, Erradicador,
+            Apocalypse, Superman Ciborgue, General Zod, Superboy Primordial,
+            Imperiex, Caveira Atômica, Sanguinário I, Sanguinário II, Blaze,
+            Lorde Satanus, Barão Sunday, Bomba-Relógio, Homem-Brinquedo, etc...{" "}
           </p>
         </div>
         <div>
@@ -38,11 +56,15 @@ function App() {
             descobriu que tinha capacidades sobre-humanas. Quando não está com o
             tradicional uniforme azul e vermelho, vive com Lois Lane e Jimmy
             Olsen como Clark Kent, um repórter/jornalista no Planeta Diário.
+            Krypton è o planeta natal de Kal-El, da Supergirl, da Poderosa, e
+            vários outros kryptonianos. Lá viviam muitas e diversas espécies de
+            animais. Atualmente não existe mais, pois ele explodiu devido a
+            instabilidade de seu núcleo.
           </p>
           <p></p>
         </div>
       </div>
-      <p>criador: Kalel P.</p>
+      <p>Criador: Kalel P.</p>
     </div>
   );
 }
